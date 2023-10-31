94017b7b0c87f5852529a312d0e9e569a7bb9dbb
LyroudisK <s232994@dtu.dk>
2023-10-31 09:01:49 +0000

    First module name fix

--- R/mod_golem_module.R
+++ R/mod_golem_module.R
@@ -1,31 +1,31 @@
-#' golem_moduls UI Function
+#' golem_module UI Function
 #'
 #' @description A shiny Module.
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
-mod_golem_moduls_ui <- function(id){
+#' @importFrom shiny NS tagList
+mod_golem_module_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+
   )
 }
-    
-#' golem_moduls Server Functions
+
+#' golem_module Server Functions
 #'
-#' @noRd 
-mod_golem_moduls_server <- function(id){
+#' @noRd
+mod_golem_module_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
-# mod_golem_moduls_ui("golem_moduls_1")
-    
+# mod_golem_module_ui("golem_module_1")
+
 ## To be copied in the server
-# mod_golem_moduls_server("golem_moduls_1")
+# mod_golem_module_server("golem_module_1")
